09ccd2c15a507082a8bc6cb35e6a225ef3f1213a
Lauri Leskinen <laurileskinen@icloud.com>
2021-04-04 20:53:24 +0300

    Updated whitenoise STATICFILES_STORAGE parameter

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
